ff4023dd1b1eb9628af151ba4dc01bad79281fe8
AerinLom <aerinlom1@gmail.com>
2024-07-23 11:20:44 +0000

    Task 9

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,15 @@
+
+export default function guardrail(mathFunction) {
+  const queue = [];
+  
+  try {
+    const answer = mathFunction();
+    queue.push(answer);
+  } catch (error) {
+    queue.push(`Error: ${error.message}`);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  
+  return queue;
+}
