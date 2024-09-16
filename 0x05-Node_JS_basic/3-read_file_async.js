2b399ce8d1e61ad2abfd732a06fd9e6dc69b117c
AerinLom <aerinlom1@gmail.com>
2024-09-16 08:07:37 -0700

    Task 3: Async file reading

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,47 @@
+const fs = require('fs').promises;
+
+function countStudents(path) {
+  return fs.readFile(path, 'utf8')
+    .then((data) => {
+      const lines = data.trim().split('\n').filter(line => line);
+
+      if (lines.length <= 1) {
+        throw new Error('Cannot load the database');
+      }
+
+      const header = lines[0].split(',');
+
+      const studentsByField = {};
+
+      for (let i = 1; i < lines.length; i++) {
+        const student = lines[i].split(',');
+
+        if (student.length !== header.length) continue;
+
+        const firstName = student[0];
+        const field = student[3];
+
+        if (!studentsByField[field]) {
+          studentsByField[field] = [];
+        }
+        studentsByField[field].push(firstName);
+      }
+
+      const totalStudents = Object.values(studentsByField).reduce(
+        (acc, students) => acc + students.length,
+        0
+      );
+      console.log(`Number of students: ${totalStudents}`);
+
+      for (const [field, students] of Object.entries(studentsByField)) {
+        console.log(
+          `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`
+        );
+      }
+    })
+    .catch((err) => {
+      throw new Error('Cannot load the database');
+    });
+}
+
+module.exports = countStudents;
