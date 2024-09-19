831d321171de56a8ab66ad313cdb378b92deed17
AerinLom <aerinlom1@gmail.com>
2024-09-19 03:50:12 -0700

    Task 9: Regex int testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,25 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Index page', () => {
+  const API_URL = 'http://localhost:7865';
+
+  it('returns correct message and status code', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('Cart page', () => {
+  const API_URL = 'http://localhost:7865';
+
+  it('should return status code 200 and correct message when id is a number', (done) => {
+    request.get(`${API_URL}/cart/123`, (_err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 123');
+      done();
+    });
+  });
